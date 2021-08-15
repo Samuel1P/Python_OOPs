482cde454cd58a63b61e8db8564704da7df3bb32
Samuel I P <ipsamuelip@gmail.com>
2021-08-16 01:01:05 +0530

    getting started with section 1 project

--- 1_Classes/class_type.py
+++ 1_Classes/class_type.py
@@ -1,16 +1,29 @@
+from types import FunctionType
 class Person:
     pass
+
+def func():
+    pass
+
 obj = Person()
 print (type(Person))
+print (Person)
+
+print (type(Person()))
+print (Person())
 print(type(obj))
 print(obj.__class__)
 print(Person.__name__)
 print (type(str))
+print (type(func))
+print (isinstance(func, FunctionType))
 """
 <class 'type'>
 <class '__main__.Person'>
 <class '__main__.Person'>
+<class '__main__.Person'>
 Person
 <class 'type'>
-
+<class 'function'>
+True
 """
