7e8e87f828d5e5a2606e1bd1fe4fa43e87f3c3b1
Samuel I P <ipsamuelip@gmail.com>
2022-06-26 23:36:05 +0530

    Got a working test logging model for the repo. Refactor coming in next commit.

--- tests/TestLogger.py
+++ tests/TestLogger.py
@@ -5,29 +5,39 @@ import logging
 from datetime import datetime
 import os
 import sys
-from logging import FileHandler
+from logging import FileHandler, StreamHandler
 import traceback
 # from tests.FrameworkLogger import INFO, ERROR, configure
 
 def log_info(msg):
-  logger = (logging_tests)
+  logger = (logging.tests)
   logger.info(msg, extra=__extra())
 
+def log_debug(msg):
+  logger = (logging.tests)
+  logger.debug(msg, extra=__extra())
+
 def log_error(msg):
-  logger = (logging_tests)
+  logger = (logging.tests)
   logger.error(msg, extra=__extra())
   
 def log_warn(msg):
-  logger = (logging_tests)
-  logger.warn(msg, extra=__extra())
+  logger = (logging.tests)
+  logger.warning(msg, extra=__extra())
   
 def log_critical(msg):
-  logger = (logging_tests)
+  logger = (logging.tests)
   logger.critical(msg, extra=__extra())
+  
+def log_exception(msg):
+  logger = (logging.tests)
+  logger.exception(msg, extra=__extra())
+  
 def __extra():
-  """Function to pass the callers name and line number.
+  """
+  Function to pass the callers name and line number.
   Returns:
-    A dictionary with 'stage', 'step' and 'file_line' details
+    A dictionary with file name and file line
   """
   frame = traceback.extract_stack()[-3]
   file_name = frame[0].split("/")[-1]
@@ -44,31 +54,30 @@ def get_logger_instance(name):
     Returns:
         logger: logger obj
     """
-    global logging_tests
-    logging_tests = logging.getLogger(name)
-    #logging.tests.handlers = []
-    #logging.getLogger().disabled = True
-    ROOT_DIR = "/Users/samuel.ip/Documents/Workspace/Learning_Workspace/Python_OOPs/tests"
-    path = datetime.now().strftime('%Y-%m-%d-%f.log')
+    logging.tests = logging.getLogger(name)
+    logging.tests.setLevel(logging.DEBUG)
+
+    
     WO_FUNCTION_FORMAT = '[%(asctime)s,%(msecs)03d] [%(levelname)s] [%(threadName)s] [%(file_line)s] [%(filename)s:%(lineno)s-%(funcName)s()] %(message)s'
     formatter = logging.Formatter(WO_FUNCTION_FORMAT, "%Y-%m-%d %H:%M:%S")
     
-    debug_handler = FileHandler(os.path.join(ROOT_DIR, 'test_logs', name +'_' + path), 'a')
+    ROOT_DIR = "/Users/samuel.ip/Documents/Workspace/Learning_Workspace/Python_OOPs/"
+    file_name_suffix = datetime.now().strftime('%Y-%m-%d-%f.log')
+    
+    debug_handler = FileHandler(os.path.join(ROOT_DIR, 'tests', 'test_logs', name +'_' + file_name_suffix), 'a')
+    debug_handler.set_name('debug_file')
     debug_handler.setLevel(logging.DEBUG)
     debug_handler.setFormatter(formatter)
-    #debug_handler.set_name('primary')
-
-    logging_tests.setLevel(logging.DEBUG)
-    logging_tests.addHandler(debug_handler)
+    logging.tests.addHandler(debug_handler)
+    
+    console_handler = StreamHandler(sys.stdout)
+    
+    console_handler.set_name('console_stream')
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logging.tests.addHandler(console_handler)
     #logging.tests.propagate = 0
-    
-    
 
 
-# def help_logger(name, input_args={}):
-#     folder = "test_logs/"+str(name)
-#     log_name = "{0}_{1}.log".format(
-#     name, datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-#     configure(log_dir=folder, log_file=log_name, level="DEBUG")
-#     INFO("Script Inputs : {}".format(input_args))
-#     INFO("Log file : {0}/{1}".format(folder, log_name))
+
+
